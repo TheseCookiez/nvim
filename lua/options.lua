17c1be57e79f6b0f76c08ae9b46fbac9f2d069cd
MathiasAase <137276691+MathiasAase@users.noreply.github.com>
2025-09-24 10:53:35 +0200

    Updated lspconfig and nvim-dap
    * Updated lspconfig & nvim-dap

--- lua/options.lua
+++ lua/options.lua
@@ -1,4 +1,5 @@
 require "nvchad.options"
 
+vim.fn.sign_define("DapBreakpoint", { text = "🛑", texthl = "", linehl = "", numhl = "" })
 -- add yours here!
 -- o.cursorlineopt ='both' -- to enable cursorline!
