ae1d8148427295994a8d70959f9d256257d1865b
maa <maa@securenok.com>
2026-02-11 09:06:18 +0000

    Make relatvie line number default

--- lua/options.lua
+++ lua/options.lua
@@ -1,5 +1,8 @@
 require "nvchad.options"
 
 vim.fn.sign_define("DapBreakpoint", { text = "🛑", texthl = "", linehl = "", numhl = "" })
+vim.wo.number = true
+vim.wo.relativenumber = true
+
 -- add yours here!
 -- o.cursorlineopt ='both' -- to enable cursorline!
