7c51e5d503c90a1ba4c3f420788e4c194b064504
maa <maa@securenok.com>
2026-02-16 10:33:21 +0000

    Added ruff lsp by default

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "clangd", "cssls", "pyright", "zls", "gopls", "ts_ls", "html" }
+local servers = { "clangd", "cssls", "pyright", "zls", "gopls", "ts_ls", "html", "ruff" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
